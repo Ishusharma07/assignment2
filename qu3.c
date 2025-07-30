2eba54f5227e1dcca81df91712eeade2287a125c
ishu sharma <ishu83543@gmail.com>
2025-07-31 00:29:18 +0530

    q = 3 ans = 3
    Signed-off-by: ishu sharma  <ishu83543@gmail.com>

---
+++ qu3.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+void main(){
+	int a = 900;
+	int b = 300;
+	int c = 10;
+	if(a>b){
+		if(a>c)
+		printf("a is greater");
+		else
+			printf("c is  greater");
+		} else{
+			if(b > c)
+			printf("b is greater");
+			else{
+				printf("c is greater");
+			}
+	}
+}

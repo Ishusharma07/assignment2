e2c20fe5a3b54a7d4942f938826cd75fca875439
ishu sharma <ishu83543@gmail.com>
2025-07-31 00:27:39 +0530

    q = 1 , ans = 1
    Signed-off-by: ishu sharma  <ishu83543@gmail.com>

---
+++ qu1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void main(){
+	int a = 14;
+	int b = 12;
+	
+	printf("a+b= %d\n", a+b);	
+	printf("a-b= %d\n", a-b);	
+	printf("a*b= %d\n", a*b);	
+	printf("a/b= %d\n", a/b);
+	printf("a%b= %d\n", a%b);		
+}
